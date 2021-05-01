feec2b807273b62e5a7e2fa6be44cb7e44aa923c
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2021-05-01 10:42:37 +0000

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app  = express()
+app.use(express.json())
+
+var covid_data = []
+
+app.get('/', function(req,res){
+    res.send(covid_data)
+})
+
+app.listen(8080, ()=> console.log("Server is running"))
+
+app.post('/',function(req,res){
+    var data = req.body
+    console.log(data);
+    CovidData.push(data);
+    res.send("Data  uploaded")
+})
