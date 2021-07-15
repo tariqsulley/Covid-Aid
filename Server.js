99438b5a9933440c23738cb8f3909d63f27d8f82
Prince Asiedu <princekofasiedu@gmail.com>
2021-07-15 21:34:43 +0100

    Modified server.js

--- Server.js
+++ Server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app  = express()
-app.use(express.json())
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-var covid_data = []
+var covid_data = [];
 
-app.get('/', function(req,res){
-    res.send(covid_data)
-})
+app.get("/", function (req, res) {
+  res.send(covid_data);
+});
 
-app.listen(8080, ()=> console.log("Server is running"))
-
-app.post('/',function(req,res){
-    var data = req.body
+app.post("/", function (req, res) {
+    var data = req.body;
     console.log(data);
     covid_data.push(data);
-    res.send("Data  uploaded")
-})
+    res.send("Data  uploaded");
+  });
+  
+app.listen(8080, () => console.log("Server is running"));
